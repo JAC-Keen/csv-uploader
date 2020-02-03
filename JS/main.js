5913e069cb2e82f1761823810e8544f011b95af6
JAC-Keen <jorge.cano@keen.io>
2020-02-03 16:05:42 -0600

    test file and csv convert solution

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,36 @@
-# AJAX / JQUERY
-# Create a dynamic URL
-# Take the Project ID, Write Key, Name of the Collection inputs from the fields
-# and push those entries into the URL that will create the Post Request
-# return the response success of failed
+// AJAX / JQUERY
+// Create a dynamic URL
+// Take the Project ID, Write Key, Name of the Collection inputs from the fields
+// and push those entries into the URL that will create the Post Request
+// return the response success of failed
+
+
+
+//var csv is the CSV file with headers
+function csvJSON(csv){
+
+  var lines=csv.split("\n");
+
+  var result = [];
+
+  // NOTE: If your columns contain commas in their values, you'll need
+  // to deal with those before doing the next step 
+  // (you might convert them to &&& or something, then covert them back later)
+  // jsfiddle showing the issue https://jsfiddle.net/
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+      var obj = {};
+      var currentline=lines[i].split(",");
+
+      for(var j=0;j<headers.length;j++){
+          obj[headers[j]] = currentline[j];
+      }
+
+      result.push(obj);
+
+  }
+
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
