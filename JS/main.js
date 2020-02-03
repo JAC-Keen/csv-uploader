a01642c6a1691a2af0a4a2d877b3a34dd144ceea
JAC-Keen <jorge.cano@keen.io>
2020-02-03 13:24:05 -0600

    js file

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+# AJAX / JQUERY
+# Create a dynamic URL
+# Take the Project ID, Write Key, Name of the Collection inputs from the fields
+# and push those entries into the URL that will create the Post Request
+# return the response success of failed
